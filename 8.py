f09368677798471896d8a48c4380f8aba963f675
Mohammednaeem Patel <monaeem0112@gmail.com>
2025-04-16 23:22:07 -0400

    formating

--- 8.py
+++ 8.py
@@ -1,7 +1,7 @@
 primeNum = [2, 5, 7, 11, 13, 17]
 oddNum = [1, 3, 5, 7, 9, 11, 13, 15, 17]
 
-common=[]
+common = []
 
 for num in primeNum:
     if num in oddNum:
@@ -13,11 +13,11 @@ print(common)
 a = [2, 4, 6, 8, 10, 12]
 b = [3, 6, 9, 12, 15, 18]
 
-cmn=[]
+cmn = []
 
 for num in a:
     if num in b:
-        if num%3==0:
+        if num % 3 == 0:
             cmn.append(num)
 
 print(cmn)
--- 9.py
+++ 9.py
@@ -1,4 +1,4 @@
-for i in range(10,100):
-        i = str(i)
-        if int(i[0])<int(i[1]):
-            print(i, end="  ")
+for i in range(10, 100):
+    i = str(i)
+    if int(i[0]) < int(i[1]):
+        print(i, end="  ")
