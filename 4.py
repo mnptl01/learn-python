6422ab648ad6b7afbe4bd9ca3b579f8915d18604
Mohammednaeem Patel <monaeem0112@gmail.com>
2025-04-16 23:20:03 -0400

    practiced loops = more concepts clear

--- 4.py
+++ 4.py
@@ -1,9 +1,9 @@
 numbers = [10, 20, 30, 40, 50]
 output = []
-i=0
+i = 0
 sum = 0
-while(i<len(numbers)):
+while (i < len(numbers)):
     sum += numbers[i]
     output.insert(i, sum)
     i += 1
-print(output)
+    print(output)
---
+++ 8.py
@@ -0,0 +1,23 @@
+primeNum = [2, 5, 7, 11, 13, 17]
+oddNum = [1, 3, 5, 7, 9, 11, 13, 15, 17]
+
+common=[]
+
+for num in primeNum:
+    if num in oddNum:
+        common.append(num)
+
+print(common)
+
+
+a = [2, 4, 6, 8, 10, 12]
+b = [3, 6, 9, 12, 15, 18]
+
+cmn=[]
+
+for num in a:
+    if num in b:
+        if num%3==0:
+            cmn.append(num)
+
+print(cmn)
