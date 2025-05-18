bfb9ed9669ad513127f9ec2e6aad27d1fa5fc9a8
Mohammednaeem Patel <monaeem0112@gmail.com>
2025-05-17 20:02:17 -0400

    formatting

--- 36.py
+++ 36.py
@@ -15,7 +15,8 @@ class Customer:
         print(f"Available balance in the account is {self.Balance}")
 
 
-Acc1 = Customer(100, 100000, 1000)
+Acc1 = Customer(100, 100000)
+print(Acc1)
 print(Acc1.accNo)
 print(Acc1.Balance)
 Acc1.credit(1000)
